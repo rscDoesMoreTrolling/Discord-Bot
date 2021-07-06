c671fa3797600b2e20f175b878c611c145aa830a
NekoIceCream <nekoicecream@outlook.co.id>
2021-07-06 13:25:37 +0700

    Testing

--- index.js
+++ index.js
@@ -32,14 +32,14 @@ client.on('message', async message =>{
 client.login(token)
 
 //testing afk command
-if(db.has(`afk-${message.author.id}+${message.guild.id}`)) {
-        const info = db.get(`afk-${message.author.id}+${message.guild.id}`)
-        await db.delete(`afk-${message.author.id}+${message.guild.id}`)
+if(db.has(`afk.${message.author.id}+${message.guild.id}`)) {
+        const info = db.get(`afk.${message.author.id}+${message.guild.id}`)
+        await db.delete(`afk.${message.author.id}+${message.guild.id}`)
         message.reply(`Your afk status have been removed (${info})`)
     }
     if(message.mentions.members.first()) {
-        if(db.has(`afk-${message.mentions.members.first().id}+${message.guild.id}`)) {
-            message.channel.send(message.mentions.members.first().user.tag + ":" + db.get(`afk-${message.mentions.members.first().id}+${message.guild.id}`))
+        if(db.has(`afk.${message.mentions.members.first().id}+${message.guild.id}`)){
+            message.channel.send(message.mentions.members.first().user.tag + "
+        }
         }else return;
     }else;
-//testing
