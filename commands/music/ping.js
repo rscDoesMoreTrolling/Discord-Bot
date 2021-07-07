18f95c2d442e59204ab43e0ba381dcfe08a0ccf1
NekoIceCream <nekoicecream@outlook.co.id>
2021-07-07 20:54:06 +0700

    Patch_5

--- commands/music/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name : 'ping',
-    run : async(client, message, args) => {
-
-    message.channel.send(`${client.emotes.success} - Ping : **${client.ws.ping}ms** !`)
-  }
-};
