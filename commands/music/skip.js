7d1c4ccddb5fa54dafb9fdb404f9966d1952ffa4
GameWatch21 <43901265+GameWatch21@users.noreply.github.com>
2021-07-07 12:26:37 +0700

    update

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,4 +1,6 @@
-exports.run = async (client, message) => {
+module.exports = {
+    name : 'skip',
+    run : async(client, message, args) => {
 
     if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
@@ -7,5 +9,5 @@ exports.run = async (client, message) => {
     client.player.skip(message);
 
     message.channel.send(`${client.emotes.success} - The current music has just been **skipped** !`);
-
+  }
 };
