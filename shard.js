ca49a769284f4ad4371909763841e319fd2167aa
NekoIceCream <nekoicecream@outlook.co.id>
2021-07-09 10:47:56 +0700

    Patch$8

--- shard.js
+++ shard.js
@@ -2,7 +2,7 @@ const { ShardingManager } = require('discord.js');
 const config = require('./config.json');
 const bumbe = new ShardingManager('./index.js',
 { 
-	totalShards: config.shard,
+	totalShards: 1,
     token: config.token
 });
 bumbe.spawn();
