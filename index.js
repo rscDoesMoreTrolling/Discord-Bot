e6691128083cd117703b71cca0763ff0eb084ddb
NekoIceCream <nekoicecream@outlook.co.id>
2021-07-09 13:42:17 +0700

    Patch_8

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {Collection, Client, Discord} = require('discord.js')
 const fs = require('fs')
-const client = new Client({
+const client = new Client
+({
     disableEveryone: false
 })
 const config = require('./config.json');
