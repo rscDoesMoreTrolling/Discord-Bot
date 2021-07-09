80106a118c13ff8b90e81f792780eae7f9a8c7d5
GameWatch21 <43901265+GameWatch21@users.noreply.github.com>
2021-07-09 19:59:05 +0700

    Fix

--- commands/moderator/set-chatbot.js
+++ commands/moderator/set-chatbot.js
@@ -2,8 +2,7 @@ const { client, message, MessageEmbed } = require('discord.js');
 const schema = require('../../models/chatbot-channel');
 module.exports = {
     name : 'set-chatbot',
-    category : 'Fun',
-    timeout : 1,
+    usage: "<msg>",
     description : 'chat bot command to chatting with me!',
 
     /**
@@ -12,7 +11,7 @@ module.exports = {
      * @param {String[]} args
      */
 
-    run : async(client, message, args) => {
+    run : async(bot, message, args) => {
         if(!message.member.permissions.has("ADMINISTRATOR")) return message.channel.send("You dont have permission to use this command sadly :(");
         const channel = message.mentions.channels.first() || message.channel;
         schema.findOne({ Guild: message.guild.id}, async (err, data) => {
