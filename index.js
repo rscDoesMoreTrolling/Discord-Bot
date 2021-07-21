315ff30243ddcf015bb6faf902a32b773dc62c11
NekoIceCream <nekoicecream@outlook.co.id>
2021-07-21 21:14:34 +0700

    Patch_9

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ client.once('ready', () =>
 
 client.on('message', async message =>
 {
-    schema.findOne({ Guild: message.guild.id}, async (err, data) => 
+    Schema.findOne({ Guild: message.guild.id}, async (err, data) => 
     { 
     //if(!data) return message.channel.send("no data"); 
     if (message.channel.id !== data.Channel) return; 
